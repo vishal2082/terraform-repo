683b2f12baea1d2f5816cc0099b55c6c2a307b70
vishal2082 <35956311+vishal2082@users.noreply.github.com>
2024-07-13 16:23:33 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,6 @@ provider "aws" {
 resource "aws_security_group" "my_security_group" {
   name = var.security_group
   description = "security group for Ec2 instance"
-}
 
   ingress {
    from_port = 8080
